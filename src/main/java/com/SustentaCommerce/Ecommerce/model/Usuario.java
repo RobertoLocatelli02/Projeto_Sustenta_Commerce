9d33f06775e4df672f661f5aa96014dfe94600bc
Roberto Locatelli <rlocatelli02@gmail.com>
2021-06-07 21:05:26 -0300

    develop

--- src/main/java/com/SustentaCommerce/Ecommerce/model/Usuario.java
+++ src/main/java/com/SustentaCommerce/Ecommerce/model/Usuario.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "tb_usuario", uniqueConstraints=@UniqueConstraint(columnNames={"email, usuario"}))
+@Table(name = "tb_usuario", uniqueConstraints=@UniqueConstraint(columnNames={"email"}))
 public class Usuario {
 	
 	Usuario() {
